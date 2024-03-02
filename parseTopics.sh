8940fd24b82034a3c2932e5e509cd0b0067be354
Adam Lechnos <email@adamlechnos.com>
2024-03-01 20:40:04 -0500

    added repoParser script to parse list of repos from repos.md and output to topics.md, every commit

--- parseTopics.sh
+++ parseTopics.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
+# Peforms a parse of all topics from repos.md and outputs to topics.md file for reference by repos.md Topics link.
+# This script executes as part of the Git pre-commit hook.
+
+date=$(date +%m/%d/%Y)
+
 scriptpath="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo $scriptpath
+#echo $scriptpath
 
-rm topics.md
+if [ ! -f $scriptpath/topics.md]
+then
+    touch $scriptpath/topics.md
+fi
 
-cat << EOF > topics.md
+rm $scriptpath/topics.md
+
+cat << EOF > $scriptpath/topics.md
 ---
 layout: page
 title: GitHub Topics
 permalink: /topics/
 ---
 
+A list of searchable topics parsed across all repos
+
 EOF
 
-cat repos.md | grep -o -P '(?<=topic%3A).*?(?=\&)' | sed 's/+topic%3A/\n/g' | sort -u | awk '{print "* " $0}' >> topics.md
+cat $scriptpath/repos.md | grep -o -P '(?<=topic%3A).*?(?=\&)' | sed 's/+topic%3A/\n/g' | sort -u | awk '{print "* " $0}' >> $scriptpath/topics.md
+echo '' >> $scriptpath/topics.md
+echo "<sub>Last Updated: $date<sub>" >> $scriptpath/topics.md
---
+++ topics.md
@@ -0,0 +1,33 @@
+---
+layout: page
+title: GitHub Topics
+permalink: /topics/
+---
+
+A list of searchable topics parsed across all repos
+
+* aws
+* bash
+* devops
+* financial
+* for-fun
+* game
+* google-apps-script
+* google-sheets
+* groovy
+* invoicing
+* java
+* javascript
+* jekyll
+* jenkins
+* json
+* linux
+* monster-alerts
+* project-management
+* python
+* ruby
+* terraform
+* web-app
+* wiki
+
+<sub>Last Updated: 03/01/2024<sub>
